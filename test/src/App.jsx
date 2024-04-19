7b251fc818c15e9a5b56ec69e5ed90e798502d23
Avikam <avikam198.be22@chitkara.edu.in>
2024-04-19 15:35:49 +0530

    Assingnment_Submission

---
+++ test/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import Header from './file/Header'
+import Notes from './file/Notes'
+
+
+
+function App() {
+  return (
+    <div className='main'>
+      <Header />
+      <Notes />
+    </div>
+  )
+}
+
+export default App
